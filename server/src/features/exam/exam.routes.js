55a537f395b62c9784e5b037399e58db701ea50b
Aditya Raj <aditya13raja@gmail.com>
2026-07-23 14:53:52 +0530

    minor(backend): secure create, update and delete routes for exam to be accessed only by admin

--- server/src/features/exam/exam.routes.js
+++ server/src/features/exam/exam.routes.js
@@ -6,10 +6,16 @@ import {
     updateExam,
     deleteExam,
 } from './exam.controller.js';
+import {requireAuth} from "../../common/middleware/auth.middleware.js";
+import {requireRole} from "../../common/middleware/role.middleware.js";
 
 const router = express.Router();
 router.get('/', getAllExams);
 router.get('/:id', getExamById);
+
+router.use(requireAuth);
+router.use(requireRole('admin'));
+
 router.post('/', createExam);
 router.patch('/:id', updateExam);
 router.delete('/:id', deleteExam);
